4835d78b75b114e6df4d5ff48fb173c6ea3552ce
irenemassa03 <114859857+irenemassa03@users.noreply.github.com>
2022-11-24 21:58:42 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,15 @@
 using namespace std;
 
 int main() {
+int n=0, a=1, b=1, c=0;
+cin >> n;
+cout << a << endl;
+cout << b << endl;
+for (int i=n; i<=n; i++){
+c=a+b;
+a=b;
+b=c;
+cout << c << endl;
+}
    return 0;
 }
